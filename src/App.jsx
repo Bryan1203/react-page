5a2bcefb1334556b3363dbc4b06fb9f1c06db131
Bryan1203 <bryanchang1203@hotmail.com>
2023-12-17 15:25:13 -0600

    added starva embedded data into blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Homepage from './components/Homepage';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MapDemo from './components/MapDemo';
 
 function App() {
   return <div><Homepage/></div>
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,38 @@
-import { Card, Container } from "react-bootstrap"
+import { useEffect } from 'react';
+import { Card, Container } from 'react-bootstrap';
 
 const Blog = () => {
-    return <Container>
-        <p>under construction</p>
-    </Container>
-}
-export default Blog
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://strava-embeds.com/embed.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Blog Title</Card.Title>
+                    <Card.Text>
+                        This is a sample blog post content.
+                    </Card.Text>
+                    <div
+                        className="strava-embed-placeholder"
+                        data-embed-type="activity"
+                        data-embed-id="7588740005"
+                        data-style="dark"
+                        data-width="10%" // Adjust width as needed
+                        style={{ maxWidth: '100px', margin: '0 auto' }} // Additional styling for responsiveness
+                    ></div>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+};
+
+export default Blog;
